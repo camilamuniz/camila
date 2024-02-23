6a3e30e26825259b477bb1c4dbb69aea9922ce0c
camilamuniz <160981013+camilamuniz@users.noreply.github.com>
2024-02-23 01:35:49 +0000

    camila

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,7 +1,14 @@
 package primeiro_exemplo;
 
+import modelos.Pessoa;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Funciona!!!");
+
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Camila");
+
+        System.out.println(p1.getNome());
     }
 }
